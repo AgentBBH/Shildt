e63884a187c644497ab9597fe08dde9c7a96fd72
Marina Smit <marina2013@tut.by>
2021-04-04 20:13:41 +0300

    lesson four 04.04.2021

---
+++ src/lessonFour/DoWhile.java
@@ -0,0 +1,13 @@
+// page 135 - 136
+
+package lessonFour;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System.out.println("такт " + n);
+        } while (--n > 0);
+    }
+}
