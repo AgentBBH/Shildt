746e2eae81e1f69f6c5a91c757f55134ac978679
Marina Smit <marina2013@tut.by>
2021-04-07 11:09:13 +0300

    lesson four 07.04.2021

--- src/lessonFour/Class/BoxDemo.java
+++ src/lessonFour/Class/BoxDemo.java
@@ -4,18 +4,30 @@ class Box {
     double width;
     double height;
     double depth;
+
+    Box(double w, double h, double d) {
+        System.out.println("Конструирование объекта Box");
+        this.width = w;
+        this.height = h;
+        this.depth = d;
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
 }
 
 class BoxDemo {
     public static void main(String[] args) {
-        Box myBox = new Box();
+        Box myBox = new Box(10, 20, 15);
+        Box myBox2 = new Box(3, 6, 9);
         double vol;
 
-        myBox.width = 10;
-        myBox.height = 20;
-        myBox.depth = 15;
-        vol = myBox.width * myBox.height * myBox.depth;
-        System.out.println("Объём равен " + vol);
+        vol = myBox.volume();
+        System.out.println("Объём равен: " + vol);
+
+        vol = myBox2.volume();
+        System.out.println("Объём равен: " + vol);
     }
 
 }
